4cd1b0d67ade103ca52a4e5484479157816ab6db
Gyeong a Kim <alwayz0121@gmail.com>
2022-06-20 05:37:45 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -2,19 +2,19 @@ const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
 const API_KEY = "dfa67fce476dcdd1d34f28afe27278f9";
 
-function onGeoOk(position){
+function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
-    .then(response => response.json()) //url을 fetch하기
+    .then((response) => response.json()) //url을 fetch하기
     .then((data) => {
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`; 
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
     }); //weather은 array라서 [0]작성해줌
-  }
-function onGeoError(){
-  alert("Can't find you. No weather for you.");
+}
+function onGeoError() {
+  alert("If you want to get weather, please accept location.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError); //getCurrentPosition(잘 되었을 때, 안 되었을 때)
